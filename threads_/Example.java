3b97d49dcff15f819e669467cda7befc059d4289
chinthaka20010226 <prabathchinthaka361@gmail.com>
2023-12-11 16:10:51 +0530

    Threads by Runnable interface

--- threads_/Example.java
+++ threads_/Example.java
@@ -138,3 +138,57 @@ import java.util.*;
 
 
 
+// ================================================================================================
+// ---> Runnable,
+
+
+class Demo{
+    public static void main(String args[]){
+        SubjectThread sub1Thread=new SubjectThread();
+        sub1Thread.threadName="T1";
+
+        Thread t1=new Thread(sub1Thread);
+
+        t1.start();
+
+        SubjectThread sub2Thread=new SubjectThread();
+        sub2Thread.threadName="T2";
+
+        Thread t2=new Thread(sub2Thread);
+
+        t2.start();
+
+        try{
+            Thread.sleep(5000);
+        }
+        catch(InterruptedException e){
+            System.out.println("Main thread interrupted");
+        }
+
+        // sub1Thread.interrupt();
+        System.out.println("The mian thread terminated");
+    }
+}
+
+class SubjectThread implements Runnable{
+    String threadName;
+
+    Scanner input=new Scanner(System.in);
+
+    @Override
+    public void run(){
+        System.out.println("Thread is running...");
+        for(int i=0;i<5;i++){
+            System.out.println(this.threadName+" i : "+i);
+
+            try{
+                Thread.sleep(5000);
+            }
+            catch(InterruptedException e){
+                System.out.println(this.threadName+" thread is interrupted");
+                // return;
+            }
+        }
+
+    }
+}
