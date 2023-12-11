9af95f4722993a3c303e647ac2b01ab455b3e235
chinthaka20010226 <prabathchinthaka361@gmail.com>
2023-12-11 16:09:53 +0530

    Threads by Thread class

--- threads_/Example.java
+++ threads_/Example.java
@@ -91,48 +91,50 @@ import java.util.*;
 
 
 
-class Demo{
-    public static void main(String args[]){
-        SubjectThread sub1Thread=new SubjectThread();
-        sub1Thread.start();
-        SubjectThread sub2Thread=new SubjectThread();
-        sub2Thread.start();
+// class Demo{
+//     public static void main(String args[]){
+//         SubjectThread sub1Thread=new SubjectThread();
+//         sub1Thread.threadName="T1";
+//         sub1Thread.start();
+//         SubjectThread sub2Thread=new SubjectThread();
+//         sub2Thread.threadName="T2";
+//         sub2Thread.start();
 
-        try{
-            Thread.sleep(5000);
-        }
-        catch(InterruptedException e){
-            System.out.println("Main thread interrupted");
-        }
+//         try{
+//             Thread.sleep(5000);
+//         }
+//         catch(InterruptedException e){
+//             System.out.println("Main thread interrupted");
+//         }
 
-        sub1Thread.interrupt();
-        System.out.println("The mian thread terminated");
-    }
-}
+//         // sub1Thread.interrupt();
+//         System.out.println("The mian thread terminated");
+//     }
+// }
 
-class SubjectThread extends Thread{
-    Scanner input=new Scanner(System.in);
+// class SubjectThread extends Thread{
+//     String threadName;
 
-    @Override
-    public void run(){
-        System.out.println("Thread is running...");
-        for(int i=0;i<5;i++){
-            System.out.println("i : "+i);
+//     Scanner input=new Scanner(System.in);
 
-            try{
-                Thread.sleep(5000);
-            }
-            catch(InterruptedException e){
-                System.out.println("Thread is interrupted");
-                return;
-            }
-        }
+//     @Override
+//     public void run(){
+//         System.out.println("Thread is running...");
+//         for(int i=0;i<5;i++){
+//             System.out.println(this.threadName+" i : "+i);
 
-    }
-}
+//             try{
+//                 Thread.sleep(5000);
+//             }
+//             catch(InterruptedException e){
+//                 System.out.println(this.threadName+" thread is interrupted");
+//                 // return;
+//             }
+//         }
+
+//     }
+// }
 
 
 
-
-// ================================================================================================
 
--- upperCasting_&_downCastig/example_.java
+++ upperCasting_&_downCastig/example_.java
@@ -29,7 +29,7 @@ class Demo{
 // =================================================================================================
 // --> UpCasting
 
-/*
+
 class A{
     void show_1(){
        System.out.println("in A show");
@@ -50,34 +50,33 @@ class Demo{
          a1.show_1(); // it's okay
     }
 }
-*/
 
 
 
 // ================================================================================================
 // DownCasting 
 
-class A{
-    void show_1(){
-       System.out.println("in A show");
-    }
-}
+// class A{
+//     void show_1(){
+//        System.out.println("in A show");
+//     }
+// }
 
-class B extends A{
-    void show_2(){
-       System.out.println("in B show");
-    }
-}
+// class B extends A{
+//     void show_2(){
+//        System.out.println("in B show");
+//     }
+// }
 
-class Demo{
-    public static void main(String args[]){
-         A a1=new B();
+// class Demo{
+//     public static void main(String args[]){
+//          A a1=new B();
 
-         B b1=(B) a1;
-         b1.show_2();
-         b1.show_1();
-    }
-}
+//          B b1=(B) a1;
+//          b1.show_2();
+//          b1.show_1();
+//     }
+// }
 
 
 
